554a867d14164a52cc732bda8ba01b2d9c921230
haroonwaheed-19 <haroonwaheed002@hotmail.com>
2022-10-13 22:29:03 +0500

    Add Solid RectanglePattern

---
+++ RectanglePattern.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class RectanglePattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Solid Rectangle
+        System.out.println("Enter a number");
+        int n = sc.nextInt(); // n for rows
+        System.out.println("enter a number");
+        int m = sc.nextInt(); // m for columns
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
